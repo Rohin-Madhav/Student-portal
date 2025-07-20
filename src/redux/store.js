12677edd9e7a05521d00a487fd41fd74f45f74b5
Rohin Madhav <rohinmadhavk7@gmail.com>
2025-07-20 19:16:50 +0530

    Add Store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import studentReducer from './studentSlice'
 
-const store = configureStore({
+const Store = configureStore({
     reducer:{
         students: studentReducer,
     },
 })
-export  default store
+export  default Storetore
